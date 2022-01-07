3246fefaf57a7e5e30cf2e0777b8d8b0353a371c
marsicoa <50459011+marsicoa@users.noreply.github.com>
2022-01-07 09:24:03 -0300

    Add files via upload

--- app-conteo/routes/index.js
+++ app-conteo/routes/index.js
@@ -1,34 +1,41 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Control = require('../public/javascripts/Control.js')
-let nuevoControl = new Control()
+const Control = require("../public/javascripts/Control.js");
+let nuevoControl = new Control();
 
 //home
-router.get('/', (req, res) => {
-    res.render('pages/index')//, {repartos: nuevoControl.reparto()} )
-})
+router.get("/", async (req, res) => {
+  res.render("pages/index", { repartos: await nuevoControl.reparto() });
+});
 
 //listado control
-router.get('/control', async (req, res) => {
-  res.render('pages/control', {listado: await nuevoControl.listar()})//, {listado: nuevoControl.listar()} )
-})
+router.get("/control", async (req, res) => {
+  res.render("pages/control", { listado: await nuevoControl.listar() });
+});
 
-router.post('/control', async (req, res) => {
-    await nuevoControl.guardar(req.body.barcode)
-    res.redirect('/')
-})
+//guardar bulto controlado
+router.post("/control", async (req, res) => {
+  await nuevoControl.guardar(req.body.barcode);
+  res.redirect("/");
+});
 
 //eliminar bulto segun id
-router.get('/control/:id/:_rev', async (req, res) => {
-  await nuevoControl.eliminar(req.params.id, req.params._rev)
-  res.redirect('/control')
-})
+router.get("/control/:id/:_rev", async (req, res) => {
+  await nuevoControl.eliminar(req.params.id, req.params._rev);
+  res.redirect("/control");
+});
 
-//nuevo conteo --EMPEZAR DESDE ACA
-router.get('/control/nuevo', async (req, res) => {
-  await nuevoControl.nuevo()
-  res.redirect('/')
-})
+//nuevo conteo
+router.get("/control/nuevo", async (req, res) => {
+  await nuevoControl.nuevo();
+  res.redirect("/");
+});
+
+//resumen de ejecucion
+router.get("/control/listado", async (req, res) => {
+  res.render("partials/elements/listadoControl", {
+    listado: await nuevoControl.listar(),
+  });
+});
 
 module.exports = router;
- 
